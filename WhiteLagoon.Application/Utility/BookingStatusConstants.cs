be89a7617e32b977d229a063b8fe27b3b0263896
Yehor Lesnevych <egor.lesnev@ukr.net>
2025-09-19 19:15:29 +0300

    Create BookingStatusConstants.cs

---
+++ WhiteLagoon.Application/Utility/BookingStatusConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace WhiteLagoon.Application.Utility;
+
+public static class BookingStatusConstants
+{
+	public const string Pending = "Pending";
+
+	public const string Approved = "Approved";
+
+	public const string CheckedIn = "CheckedIn";
+
+	public const string Completed = "Completed";
+
+	public const string Cancelled = "Cancelled";
+
+	public const string Refunded = "Refunded";
+}
