48425af87cf4b76f6d070be7ecfcfdab82e8ae0e
Yehor Lesnevych <egor.lesnev@ukr.net>
2025-09-19 19:12:52 +0300

    Add user auto populated fields

--- WhiteLagoon/Controllers/BookingController.cs
+++ WhiteLagoon/Controllers/BookingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Domain.Entities;
 
@@ -6,8 +7,19 @@ namespace WhiteLagoon.Controllers;
 
 public class BookingController(IUnitOfWork unitOfWork) : Controller
 {
+	[Authorize]
 	public async Task<IActionResult> FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
 	{
+		var claimsIdentity = User.Identity as System.Security.Claims.ClaimsIdentity;
+		var userId = claimsIdentity?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+
+		var user = await unitOfWork.Users.GetAsync(u => u.Id == userId);
+
+		if(user is null)
+		{
+			return RedirectToAction(nameof(AccountController.Login), "Account");
+		}	
+
 		var villa = await unitOfWork.Villas.GetAsync(v => v.Id == villaId, includeProperties: nameof(Villa.VillaAmenities));
 
 		if(villa is null)
@@ -22,7 +34,12 @@ public class BookingController(IUnitOfWork unitOfWork) : Controller
 			CheckInDate = checkInDate,
 			Nights = nights,
 			CheckOutDate = checkInDate.AddDays(nights),
-			TotalCost = villa.Price * nights
+			TotalCost = villa.Price * nights,
+			UserId = user.Id,
+			PhoneNumber = user.PhoneNumber,
+			Email = user.Email,
+			Name = user.Name,
+			User = user
 		};
 
 		return View(booking);
